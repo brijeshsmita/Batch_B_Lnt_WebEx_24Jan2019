f822a17c633177498024c0a583b7f5898be7570d
Smita B Kumar <brijeshsmita@gmail.com>
2018-12-18 11:39:44 +0530

    Add files via upload

---
+++ _001_Day1_Intro_10Dec/HelloWorld.java
@@ -0,0 +1,20 @@
+//public class Name and the the java file name must be same
+//java file has a extension '.java'
+public class HelloWorld{
+	public static void main(String [] args){
+		System.out.println("Hello World");
+	}
+}//single line comment (comments are the code only for programmer 
+//comment are omitted by compiler
+/* muilti-line comment
+public ,static,void,class -> all are keywords in java
+class -> used to create class in java
+static -> single copy per class
+public -> avialable outside the class as well as package
+void -> return nothing
+String -> is a class in java
+System -> is a class in java
+out -> static property of System class which deals with standard        output devices
+println -> is a method to print line on the console
+main -> is a inbuild method , which acts as a entry point to the class
+*/
